308e1ece047d6463c04d09f803c76207f651257e
Conner Toney <conrad.toney@gmail.com>
2015-05-15 00:54:00 -0400

    current status of game

---
+++ GameStatus.java
@@ -0,0 +1,11 @@
+/**********************************************************************
+Enum representation of the status of the game.
+
+@author Conner Toney
+@version GVSU Winter 2015
+ *********************************************************************/
+
+package package1;
+
+public enum GameStatus 
+	{PLAYER1_WON, PLAYER2_WON, CATS, IN_PROGRESS}
