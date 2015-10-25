8ddd1c62007a11b3d63491804d4c73ffe353d9ed
Conrad <toneyc@mail.gvsu.edu>
2015-10-24 21:02:25 -0400

    added bin and src

---
+++ src/Cell.java
@@ -0,0 +1,48 @@
+/**********************************************************************
+The Cell object acts as a tile in the board game.  A Cell is defined
+by the player's number that selected it.
+
+@author Conner Toney
+@version GVSU Winter 2015
+ *********************************************************************/
+
+public class Cell {
+
+	/** the player's number used to define a cell */
+	private int playerNumber;
+	
+	/** the current safety level of the cell */
+	private int safetyLevel;
+	
+	/******************************************************************
+    Constructor used to create cell with specified player number
+	 *****************************************************************/
+	public Cell(int playerNumber) {
+		this.playerNumber = playerNumber;
+		safetyLevel = 1;
+	}
+	
+	/******************************************************************
+    Gets the cell's player number
+    @return this cell's player number
+	 *****************************************************************/
+	public int getCellNumber() {
+		return playerNumber;
+	}
+	
+	/******************************************************************
+    Sets the safety level of this cell
+    @param safetyLevel input safety level for cell
+	 *****************************************************************/
+	public void setSafety(int safetyLevel) {
+		this.safetyLevel = safetyLevel;
+	}
+	
+	/******************************************************************
+    Gets the safety level of this cell
+    @return the safety level
+	 *****************************************************************/
+	public int getSafety() {
+		return safetyLevel;
+	}
+}
---
+++ src/GameStatus.java
@@ -0,0 +1,9 @@
+/**********************************************************************
+Enum representation of the status of the game.
+
+@author Conner Toney
+@version GVSU Winter 2015
+ *********************************************************************/
+
+public enum GameStatus 
+	{PLAYER1_WON, PLAYER2_WON, CATS, IN_PROGRESS}
